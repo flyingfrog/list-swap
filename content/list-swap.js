fc56765f5a0e482b4f69d17ce64b588ccde64c97
Nathan Gerratt <ngerratt@gmail.com>
2014-02-02 22:15:03 -0500

    Added feature-detection and warnings if features not available

---
+++ content/list-swap.js
@@ -0,0 +1,11 @@
+
+
+window.onload = function() {
+    if(sniffFeatures) {
+	document.getElementById("file_warning").style.display="none";
+    }
+
+    if(sniffFeatures()) {
+	document.getElementById("feature_warning").style.display="none";
+    }
+}
